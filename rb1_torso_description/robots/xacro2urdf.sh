21af4dcd8708069321a985005bb078d4d301c18e
rguzman <rguzman@robotnik.es>
2016-03-01 17:01:57 +0100

    urdf and meshes of the new torso. xtion x orbbec

--- rb1_torso_description/robots/xacro2urdf.sh
+++ rb1_torso_description/robots/xacro2urdf.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-rosrun xacro xacro.py rb1_torso.urdf.xacro > rb1_torso.urdf
+#rosrun xacro xacro.py rb1_torso.urdf.xacro > rb1_torso.urdf
+rosrun xacro xacro.py rb1_torso_standalone.urdf.xacro > rb1_torso_standalone.urdf
 
